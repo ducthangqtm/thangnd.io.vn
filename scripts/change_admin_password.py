263b6864bbb0cfda47a2f9bec49a1fe9a6cd4449
ducthangqtm <ducthangqtm@gmail.com>
2026-06-30 12:12:20 +0700

    doi passs

--- scripts/change_admin_password.py
+++ scripts/change_admin_password.py
@@ -1,4 +1,9 @@
 import getpass
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from werkzeug.security import generate_password_hash
 from app import create_app, db
 from app.models import User
